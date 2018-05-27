fd29cae1ec75ad1e37731152db015f104c8937be
Ifan <linyangfan940403@126.com>
2018-05-27 22:47:42 +0800

    first commit

---
+++ src/test/java/com/ifanzero/mailreciever/MailrecieverApplicationTests.java
@@ -0,0 +1,22 @@
+package com.ifanzero.mailreciever;
+
+import com.ifanzero.mailreciever.service.ReadMailService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MailrecieverApplicationTests {
+
+	@Resource
+	ReadMailService readMailService;
+	@Test
+	public void contextLoads() {
+		readMailService.readMail();
+	}
+
+}
