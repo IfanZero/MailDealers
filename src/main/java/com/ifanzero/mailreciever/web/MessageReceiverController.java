f421fd968807aaf136e27eab62a3c86819ce8539
sunxiaoteng <sunxiaoteng@gzcb.com.cn>
2018-05-29 20:07:20 +0800

    add analayzed

--- src/main/java/com/ifanzero/mailreciever/web/MessageReceiverController.java
+++ src/main/java/com/ifanzero/mailreciever/web/MessageReceiverController.java
@@ -2,6 +2,7 @@ package com.ifanzero.mailreciever.web;
 
 import cn.hutool.http.HttpRequest;
 import cn.hutool.http.HttpResponse;
+import com.ifanzero.mailreciever.dao.dto.CusServ53Request;
 import com.ifanzero.mailreciever.dao.dto.QQAdsResponse;
 import com.ifanzero.mailreciever.dao.dto.QQadsRequest;
 import lombok.extern.slf4j.Slf4j;
@@ -38,15 +39,11 @@ public class MessageReceiverController {
 
     @ResponseBody
     @RequestMapping("/cus_serv_53/message")
-    public Object add53(@RequestBody QQadsRequest request) {
+    public Object add53(@RequestBody CusServ53Request request) {
         log.info(request.toString());
         QQAdsResponse qqAdsResponse = new QQAdsResponse();
         qqAdsResponse.setCode(0);
         qqAdsResponse.setMsg("success");
-
-        /*
-        {"data":{"account_id":"123123","order_time":"2017-11-27 00:00:00","url":"http:\/\/flzhan.cn\/?r_id=123_5d43e36f&_bid=2759&isAdvanced=1","data":[{"label":"\u59d3\u540d","value":"\u817e\u8baf\u79d1\u6280\u6709\u9650\u516c\u53f8"},{"label":"\u7535\u8bdd","value":"0755 8601 3388"},{"label":"\u7701\u4efd\/\u57ce\u5e02\/\u884c\u653f\u533a","value":"\u5409\u6797\u7701|\u767d\u5c71\u5e02|\u957f\u767d\u671d\u9c9c\u65cf\u81ea\u6cbb\u53bf"},{"label":"\u591a\u9879\u9009\u62e9","value":"\u591a\u90092|\u591a\u90093"}]}}
-         */
 
         return qqAdsResponse;
     }
